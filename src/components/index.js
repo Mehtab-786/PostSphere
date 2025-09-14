1ba8fb26c67618db5695f680966f1d4f6f7f980a
Mehtab Hussain <tradercoder786@gmail.com>
2025-09-14 20:09:38 +0530

    feat: implement Header and Footer components ; reusable Button, Input, and Container components;

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Container from './container/Container.jsx'
+
 
 export {
     Footer,
-    Header
+    Header,
+    Container,
+    LogoutBtn
 };
